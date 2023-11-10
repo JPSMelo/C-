69653f10822a7671485c77ac033312d258b25ecf
JPSMelo <144021840+JPSMelo@users.noreply.github.com>
2023-11-10 11:03:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03
+{public class Item {
+		private int valor;
+		private string proprietario;
+		private string modelo;
+		public int peso { get; set;}
+		public Item(string prorietario){
+			prorietario = this.proprietario;
+			peso = 20;
+		}
+		public void aumentar_peso(int valor) {
+			peso = peso + valor;
+		}
+
+	}
+
+	class Program
+	{
+		
+
+		static void Main(string[] args)
+		{
+          Item item1 = new Item ("José");
+			item1.modelo = lixeira;
+		}
+	}
+}
